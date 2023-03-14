f79a3a462ac96ecf52f048ba1c5db3a9766e6698
Bong Jin <qhdwls013@naver.com>
2023-03-14 13:05:48 +0900

    Create project ATM

---
+++ atm/src/atm/AccountManager.java
@@ -0,0 +1,17 @@
+package atm;
+
+import java.util.ArrayList;
+
+public class AccountManager {
+	
+	private static ArrayList<Account> list = new ArrayList<Account>();
+	
+	// Account ø° ¥Î«— 
+	
+	// Create 
+	// Rred
+	// Update
+	// Delete
+	
+	
+}
---
+++ atm/src/atm/Bank.java
@@ -0,0 +1,9 @@
+package atm;
+
+public class Bank {
+	
+	private UserManager usermanager;
+	private AccountManager accountmanager;
+	
+	// Banking 관련 메소드
+}
